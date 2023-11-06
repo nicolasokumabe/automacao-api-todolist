22d8dac9dd259be2fed9a156a040f795bc126233
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-11-05 22:01:35 -0300

    Post Users Atualizado 1.0

--- api/spec/post_users_spec.rb
+++ api/spec/post_users_spec.rb
@@ -26,7 +26,52 @@ describe "POST /users" do
     end
 
     it "valida id do usuário" do
-      expect(@result.parsed_response).to eql "Usuário Já existe"
+      expect(@result.parsed_response).to eql "Erro: Usuário já existe"
+    end
+  end
+
+  context "cadastro sem username" do
+    before(:all) do
+      payload = { name: "Nicolas Kumabe", username: "", password: "pwd123" }
+
+      @result = Users.new.cadastro(payload)
+    end
+    it "valida status code" do
+      expect(@result.code).to eql 400
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response).to eql "Erro: Username é um campo obrigatório"
+    end
+  end
+
+  context "cadastro sem senha" do
+    before(:all) do
+      payload = { name: "Nicolas Kumabe", username: "nicolasokumabe", password: "" }
+
+      @result = Users.new.cadastro(payload)
+    end
+    it "valida status code" do
+      expect(@result.code).to eql 400
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response).to eql "Erro: Senha é um campo obrigatório"
+    end
+  end
+
+  context "cadastro sem nome" do
+    before(:all) do
+      payload = { name: "", username: "nicolasokumabe", password: "pwd123" }
+
+      @result = Users.new.cadastro(payload)
+    end
+    it "valida status code" do
+      expect(@result.code).to eql 400
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response).to eql "Erro: Nome é um campo obrigatório"
     end
   end
 end
